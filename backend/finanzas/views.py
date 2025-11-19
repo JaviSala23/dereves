d538ff29d15f9d2838855411cf840f826e5e5d37
JaviSala23 <javisala@gmail.com>
2025-11-19 09:34:57 -0300

    feat: Add missing import for models in views.py

--- backend/finanzas/views.py
+++ backend/finanzas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
+from django.db import models
 from django.db.models import Sum, Count, Q
 from django.utils import timezone
 from datetime import datetime, timedelta
